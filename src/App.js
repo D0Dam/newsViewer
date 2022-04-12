21f79e5702bb8f0a1f5f5331a10cc269bbca94c6
KimMinJae <smallkdb@gmail.com>
2022-04-13 04:27:27 +0900

    apply react router dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, { useState, useCallback } from 'react';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
-const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback((category) => setCategory(category), []);
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import NewsPage from './Pages/NewsPage';
 
+const App = () => {
   return (
-    <div>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </div>
+    <Routes>
+      <Route path="/" element={<NewsPage />} />
+      <Route exact path="/:category" element={<NewsPage />} />
+    </Routes>
   );
 };
 
